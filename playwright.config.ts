d41bb64f6ec80cd163a1e11103c3c65e80e56e10
Karen Robles <kqaren30@gmail.com>
2026-01-27 21:08:26 -0600

    npm run build

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   webServer: {
-    command: 'npm run dev',
+    command: 'npm run build && npm run start',
     url: 'http://localhost:3000/',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
